b995d2c34ecdf50fd259b3992578d76926738d61
Jennifer DeJong <jdejong@macalester.edu>
2018-09-26 23:30:45 -0500

    added a basic input

--- ThingComparerConsole.java
+++ ThingComparerConsole.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class ThingComparerConsole {
 
   public static void main(String args[]) {
     System.out.println("Welcome to Thing Comparer (Console Edition)!");
+    System.out.println("Please input two things you would like to compare!");
+    Scanner scanny = new scanner(System.in);
+    String apple = scanny.next();
+    String orange = scanny.next();
+    System.out.println("I like " + apple +  " better. Because I said so.")
   }
 
 }

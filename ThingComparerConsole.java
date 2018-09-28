9f7b4ccfccb6f1c74c3891b4c9ab6523f4a93563
Keara Berlin <kearaberlin@gmail.com>
2018-09-27 23:28:23 -0500

    Added a 50% chance it will say one response or another.

--- ThingComparerConsole.java
+++ ThingComparerConsole.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class ThingComparerConsole {
 
   public static void main(String args[]) {
-    System.out.println("*\n*\n*\n*\n");
+    System.out.println("*\n*");
     System.out.println("Welcome to Thing Comparer (Console Edition)!");
 
     Scanner scanner = new Scanner(System.in);
@@ -15,7 +16,21 @@ public class ThingComparerConsole {
     String thingTwo = scanner.nextLine();
 
     System.out.println("Which is better - " + thingOne + " or " + thingTwo + "?");
-    System.out.println("Clearly, " + thingOne + " is better. Everyone who's anyone knows that."); 
+    System.out.println(getResponse(thingOne, thingTwo));
+  }
+
+  private static String getResponse(String thingOne, String thingTwo) {
+    Random random = new Random();
+    int randomInt = random.nextInt(2);
+    String response = "";
+    if(randomInt == 0) {
+      response = "Clearly, " + thingOne + " is better. Everyone who's anyone knows that.";
+    }
+    else {
+      response = thingTwo + " is mediocre, but I'll go with it anyway. I'm feeling spicy today.";
+    }
+
+    return response;
   }
 
 }

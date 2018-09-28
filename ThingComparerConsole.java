d533a7c4567cc4c5a8096b8e67d4cf9101d2744c
Keara Berlin <kearaberlin@gmail.com>
2018-09-27 23:08:16 -0500

    Added input and fixed response.

--- ThingComparerConsole.java
+++ ThingComparerConsole.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class ThingComparerConsole {
 
   public static void main(String args[]) {
+    System.out.println("*\n*\n*\n*\n");
     System.out.println("Welcome to Thing Comparer (Console Edition)!");
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter a thing: ");
+    String thingOne = scanner.nextLine();
+
+    System.out.println("Enter another thing: ");
+    String thingTwo = scanner.nextLine();
+
+    System.out.println("Which is better - " + thingOne + " or " + thingTwo + "?");
+    System.out.println("Clearly, " + thingOne + " is better. Everyone who's anyone knows that."); 
   }
 
 }
